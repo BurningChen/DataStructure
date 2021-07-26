aa6387fcc0a438421f2c20b2778b5448250f917d
BurningIce <13025540813@163.com>
2021-07-26 10:32:52 +0800

    堆

--- tree/src/main/java/MaxHeap/MaxHeap.java
+++ tree/src/main/java/MaxHeap/MaxHeap.java
@@ -12,7 +12,10 @@ public class MaxHeap<E extends Comparable<E>> {
     }
 
     public MaxHeap(E[] arr){
-        data = new Array<>(arr); 
+        data = new Array<>(arr);
+        for (int i = parent(arr.length -1); i >= 0 ; i--) {
+            siftDown(i);
+        }
     }
 
     public MaxHeap() {
